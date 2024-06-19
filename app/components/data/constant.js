008f9974a9012ae047d19dfd342283d20153c85b
aakash-sharma25 <as024harma@gmail.com>
2024-06-19 11:00:36 +0530

    added basic layout files

---
+++ app/components/data/constant.js
@@ -0,0 +1,68 @@
+import hero2 from "@/public/assets/hero2.svg";
+import hero3 from "@/public/assets/hero3.svg";
+import hero4 from "@/public/assets/hero4.svg";
+import hero5 from "@/public/assets/hero5.svg";
+import feature1 from "@/public/assets/feature1.jpg";
+import feature2 from "@/public/assets/feature2.jpg";
+import feature3 from "@/public/assets/feature3.jpg";
+import feature4 from "@/public/assets/feature4.jpg";
+import gallary2 from "@/public/assets/gallary2.jpg";
+import gallary3 from "@/public/assets/gallary3.jpg";
+import gallary4 from "@/public/assets/gallary4.jpg";
+import gallary5 from "@/public/assets/gallary5.jpg";
+
+export const Test = [gallary2, gallary3, gallary4, gallary5];
+export const GallaryImages = [
+  {
+    image: gallary2,
+  },
+  {
+    image: gallary3,
+  },
+  {
+    image: gallary4,
+  },
+  {
+    image: gallary5,
+  },
+];
+export const FeaturesImages = [
+  {
+    image: feature1,
+    text: "Flooting Pool",
+  },
+  {
+    image: feature2,
+    text: "Spacious Cabins Like Rooms",
+  },
+  {
+    image: feature3,
+    text: "Sea Facing Swimming Pools",
+  },
+  {
+    image: feature4,
+    text: "Gymnasium & FItness",
+  },
+];
+export const HeroFeaturesImages = [
+  {
+    image: hero2,
+    text1: "BOOK WITH",
+    text2: "ONLY 20%",
+  },
+  {
+    image: hero3,
+    text1: "PAYMENT PLAN",
+    text2: "EASY 70/30%",
+  },
+  {
+    image: hero4,
+    text1: "HANDOVER ON",
+    text2: "Q2 2027",
+  },
+  {
+    image: hero5,
+    text1: "AREA STARTS FROM",
+    text2: "700 SQMT",
+  },
+];
